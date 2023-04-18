170e18502620f315290f86552683a5290b93508c
janith <janithravi7@gmail.com>
2023-04-18 12:51:05 +0530

    2.0

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from markupsafe import escape
+from flask import render_template
 
 app = Flask(__name__)
+
+name = "Janith"
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
